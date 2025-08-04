f1344b364d075a20c1f157984a847e38e439e088
Md Vadiyat Naqvi <mdvadiyatnaqvi@gmail.com>
2025-08-04 18:18:01 +0530

    codebase updated

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app = express()
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials:true}))
+app.use(cors({ credentials: true }))
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on http:localhost:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 })
