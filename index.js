838e42ed8db71a407a1ad85171d64a96533b8ff9
Md Vadiyat Naqvi <mdvadiyatnaqvi@gmail.com>
2025-08-05 12:20:19 +0530

    home route added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-require('dotenv').config();
+require('dotenv').config({ quiet: true });
 const express = require('express');
 
 const PORT = process.env.PORT || 4000;
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Service is running');
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
