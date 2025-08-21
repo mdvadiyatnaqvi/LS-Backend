3dc9b544ee221461c331655959d2e8afed1036e7
Md Vadiyat Naqvi <mdvadiyatnaqvi@gmail.com>
2025-08-21 06:49:57 +0530

    login api created with jwt authentication

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/User");
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const signUp = async (req, res) => {
     try {
@@ -24,4 +25,38 @@ const signUp = async (req, res) => {
     }
 }
 
-module.exports = { signUp }
+const login = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const existingUser = await userModel.findOne({ email });
+        const errMsg = 'Auth Failed Email or Password is Wrong!'
+        if (!existingUser) {
+            return res.status(409).json({ message: errMsg, success: false })
+        }
+
+        const isPassEqual = await bcrypt.compare(password, existingUser.password)
+        if (!isPassEqual) {
+            return res.status(403).json({ message: errMsg, success: false })
+        }
+        const jwtToken = jwt.sign(
+            { email: existingUser.email, _id: existingUser._id },
+            process.env.JWT_SECRET,
+            { expiresIn: '24h' }
+        )
+
+        res.status(200).json({
+            message: "Login Success",
+            success: true,
+            jwtToken,
+            email,
+            name: existingUser.name
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error",
+            success: false
+        })
+    }
+}
+
+module.exports = { signUp, login }
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,11 +1,9 @@
 const { loginValidation, signupValidation } = require('../middleware/AuthValidations');
-const { signUp } = require('../controllers/AuthController')
+const { signUp, login } = require('../controllers/AuthController')
 
 const router = require('express').Router();
 
-router.post('/login', loginValidation, (req, res) => {
-    res.status(200).json({ message: 'Login successful' });
-});
+router.post('/login', loginValidation, login)
 router.post('/signup', signupValidation, signUp)
 
 module.exports = router;
